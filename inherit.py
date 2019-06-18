19b0e95f4c756f5d35893338d2ef1baa7f483b46
gayathrikasturi <50363603+gayathrikasturi@users.noreply.github.com>
2019-06-18 19:57:49 +0530

    Add files via upload

---
+++ inherit.py
@@ -0,0 +1,24 @@
+class students:
+
+	 def __init__(self,name,contact):
+	 	self.name=name
+	 	self.contact=contact hh h 
+	 def getdata(self):
+	 	 self.name=input("enter the name")
+	 	 self.contact=input("enter contact")
+	 def putdata(self):
+	 	 print("name is:"+self.name)
+	 	 print("contact:"+self.contact)
+class teacher(students):
+      def __init__(self,age):
+         self.age=age
+
+      def getage(self):
+         print("age is done")
+
+
+
+g=teacher(0)
+g.getage()
+g.getdata()
+g.putdata()
